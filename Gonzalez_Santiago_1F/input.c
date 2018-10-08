479c2a0305de6628ba50fe6469ddea6cd122ffae
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2018-10-08 21:02:24 +0000

    Update input.c

--- Gonzalez_Santiago_1F/input.c
+++ Gonzalez_Santiago_1F/input.c
@@ -11,23 +11,6 @@ static int getInt(int* pBuffer){
 static int getFloat(float* pBuffer){
     return scanf("%f",pBuffer);
 }
-
-/*static int utn_getChar(char* pBuffer){
-        fflush(stdin);
-        fgets(pBuffer,sizeof(pBuffer)-2,stdin);
-        pBuffer[strcspn(pBuffer, "\n")] = 0;
-        return 0;
-}*/
-
-/** \brief Obtiene un numero entero
- * \param *pEntero puntero a la variable int
- * \param *msg puntero al array char
- * \param *msgErr puntero al array char
- * \param int min rango de valor minimo del entero a obtener
- * \param int max rango de valor maximo del entero a obtener
- * \param id reintentos reintentos para ingresar bien el entero
- * \return Retorna (-1) si hay error, (0) si no hay error.
- */
 int utn_getEntero(  int* pEntero, char* msg,
                     char* msgErr,int min, int max,
                     int reintentos){
@@ -57,15 +40,6 @@ int utn_getEntero(  int* pEntero, char* msg,
     }
     return retorno;
 }
-/** \brief Obtiene un numero flotante
- * \param *pFlotante puntero a la variable float
- * \param *msg puntero al array char
- * \param *msgErr puntero al array char
- * \param int min rango de valor minimo del flotante a obtener
- * \param int max rango de valor maximo del flotante a obtener
- * \param id reintentos reintentos para ingresar bien el flotante
- * \return Retorna (-1) si hay error, (0) si no hay error.
- */
 int utn_getFlotante(  float* pFlotante, char* msg, char* msgErr,int min, int max,
                     int reintentos){
     int retorno=-1;
@@ -94,10 +68,6 @@ int utn_getFlotante(  float* pFlotante, char* msg, char* msgErr,int min, int max
     }
     return retorno;
 }
-/** \brief Valida si es una letra incluido el espacio
- * \param *input puntero al array char
- * \return Retorna (-1) si hay error, (1) si no hay error.
- */
 int esLetra(char* input){
     int retorno=-1,i=0;
     while (input[i]!='\0'){
@@ -112,25 +82,12 @@ int esLetra(char* input){
     }
     return retorno;
 }
-/** \brief Obtiene una cadena de caracteres
- * \param *input puntero al array char
- * \param *msg puntero al array char
- * \return No retorna nada ya que es una funcion void
- */
 void utn_getString(char* input, char* msg) {
             fflush(stdin);
             printf("%s", msg);
             fgets(input,100,stdin);
             input[strcspn(input, "\n")] = 0;
 }
-/** \brief Obtiene una cadena de caracteres validando solo letras
- * \param *input puntero al array char
- * \param *msg puntero al array char
- * \param *msgErr puntero al array char
- * \param int cant entero de la cantidad total del array
- * \param id reintentos reintentos para ingresar bien la cadena
- * \return Retorna (0) si hay error, (1) si no hay error.
- */
 int utn_getStringAvanzado(char* input,char* msg,char* msgErr,int cant, int reintentos){
     char aux[cant];
     do{
@@ -151,9 +108,6 @@ int utn_getTelefono(char* input,char* msg,char* msgErr,int cant, int reintentos)
     int i=0,retorno=-1;
     utn_getString(aux,msg);
     do{
-
-     //recorrer el vector y ver si hay alguna letra, dividiendo el numero por si mismo si da 1 es numero sino no lo es
-
         if(aux[i]=='\0'){
             retorno=0;
             break;
